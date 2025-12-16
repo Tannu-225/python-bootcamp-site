bad5defd6b34e93b2567ffbe25f82962ba7694b5
Firstgit <dynamox420@gmail.com>
2025-12-16 11:43:54 +0530

    Added project files and updates

--- scripts.js
+++ scripts.js
@@ -79,14 +79,14 @@
         });
 
         // Header Scroll Effect
-const header = document.querySelector('header');
+// const header = document.querySelector('header');
 
-window.addEventListener('scroll', () => {
-    const currentScroll = window.pageYOffset;
+// window.addEventListener('scroll', () => {
+//     const currentScroll = window.pageYOffset;
     
-    if (currentScroll > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
+//     if (currentScroll > 50) {
+//         header.classList.add('scrolled');
+//     } else {
+//         header.classList.remove('scrolled');
+//     }
+// });
