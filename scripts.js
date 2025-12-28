aa9a1d2cc9dec79d7644948785583797dcea0763
Firstgit <dynamox420@gmail.com>
2025-12-28 12:19:40 +0530

    add featues

--- scripts.js
+++ scripts.js
@@ -84,7 +84,7 @@
 // window.addEventListener('scroll', () => {
 //     const currentScroll = window.pageYOffset;
     
-//     if (currentScroll > 50) {
+//     if (currentScroll > 60) {
 //         header.classList.add('scrolled');
 //     } else {
 //         header.classList.remove('scrolled');
